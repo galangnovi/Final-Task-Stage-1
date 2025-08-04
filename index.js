a7c16c20a5aadb05cbeb91d97201078cb52e8816
galangnovi <galangnovi21@gmail.com>
2025-08-04 10:40:36 +0700

    updated

--- index.js
+++ index.js
@@ -49,6 +49,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send("Something broke!")
+})
 
 //multer  disk storage
 const storage = multer.diskStorage({
