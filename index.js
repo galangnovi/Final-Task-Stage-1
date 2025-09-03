0b7d515ed0935f02fab86802c00b62ae617b4373
galangnovi <galangnovi21@gmail.com>
2025-09-03 10:51:12 +0700

    update fix

--- index.js
+++ index.js
@@ -40,17 +40,21 @@ const app = express()
 app.use(express.urlencoded({extended:false}))
 
 
-const PgStore = pgSession(session);
+const PgStore = connectPgSimple(session);
 
 app.use(
   session({
     store: new PgStore({
       conString: process.env.DATABASE_URL,
+      tableName: "session",
+      // trik: convert expire ke integer (epoch)
+      createTableIfMissing: false,
+      pruneSessionInterval: 60,
     }),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 24 * 60 * 60 * 1000 }, // 1 hari
+    cookie: { maxAge: 24 * 60 * 60 * 1000 },
   })
 );
 
